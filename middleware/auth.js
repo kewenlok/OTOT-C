fa543c58c589e4826725887f9cea559460241fa1
kewenlok <kewen.lok@u.nus.edu>
2022-11-12 16:22:45 +0800

    Fix no token error message

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
+  let token;
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(" ")[1];
+  } catch (error) {
+    return res.status(401).send('Token is not included in Authorization header!');
+  }
 
-    if (!token) {
-      return res.status(401).send('Token is not included in Authorization header!');
-    }
-
+  try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decodedToken;
   } catch (error) {
